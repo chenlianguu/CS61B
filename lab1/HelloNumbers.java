bd4bded2c82949163155c9a2ab168f8e5c1d228e
chenliang <chenlianguu@gmail.com>
2020-04-18 10:17:09 +0800

    modify HelloNumbers.java

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -1,8 +1,10 @@
 class HelloNumbers {
     public static void main(String[] args) {
         int x = 0;
+        int sum=0;
         while (x<10) {
-            System.out.print(x+" ");
+            sum+=x;
+            System.out.print(sum+" ");
             x++;
         }
     }
